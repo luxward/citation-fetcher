8c85af7bd3b9f14690069305c03f4c5b5e5a6599
keatonLiu <lxtxiaotong@foxmail.com>
2024-12-16 18:40:48 +0800

    fix fingerprint

--- google.py
+++ google.py
@@ -13,7 +13,30 @@ ss = requests.Session()
 ss.impersonate = 'chrome124'
 ss.headers.update({
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-    'Referer': 'https://scholar.google.com/',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'cache-control': 'no-cache',
+    'pragma': 'no-cache',
+    'priority': 'u=0, i',
+    'referer': 'https://scholar.google.com/',
+    'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+    'sec-ch-ua-arch': '"x86"',
+    'sec-ch-ua-bitness': '"64"',
+    'sec-ch-ua-full-version-list': '"Google Chrome";v="131.0.6778.140", "Chromium";v="131.0.6778.140", "Not_A Brand";v="24.0.0.0"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-model': '""',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-ch-ua-platform-version': '"19.0.0"',
+    'sec-ch-ua-wow64': '?0',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'x-browser-channel': 'stable',
+    'x-browser-copyright': 'Copyright 2024 Google LLC. All rights reserved.',
+    'x-browser-validation': 'Nbt54E7jcg8lQ4EExJrU2ugNG6o=',
+    'x-browser-year': '2024'
 })
 
 h = html2text.HTML2Text()
@@ -21,9 +44,9 @@ h.ignore_tables = True
 h.ignore_links = True
 h.ignore_emphasis = True
 
-# ss.proxies.update({
-#     'https': 'http://127.0.0.1:10809',
-# })
+ss.proxies.update({
+    'https': 'http://127.0.0.1:10809',
+})
 
 converter = BibTexConverter()
 
@@ -49,7 +72,7 @@ def search_papers(q: str):
         res = ss.get(cite_url)
         try:
             text = res.text.replace(r'$\{', '').replace(r'\}$', '').replace('$', '')
-            cite = converter.convert_text(text).replace('–', '-')
+            cite = converter.convert_text(text).replace('–', '-').replace('等', 'et al')
         except RuntimeError:
             print(f'Error converting citation for {title}, {res.text}')
             return
