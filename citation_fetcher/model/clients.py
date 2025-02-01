5e41cfbec8979648a2bf3b52cd2356516856802d
liuxiangwei.lxw <liuxiangwei.lxw@alibaba-inc.com>
2025-02-01 15:26:03 +0800

    remove debug

--- citation_fetcher/model/clients.py
+++ citation_fetcher/model/clients.py
@@ -32,7 +32,7 @@ class Client:
 class BaiduClient(Client):
     @lru_cache(maxsize=30)
     def search_papers(self, q: str, limit=3, use_doi=False):
-        logger.debug(f'Searching papers for {q}')
+        #logger.debug(f'Searching papers for {q}')
         url = 'https://xueshu.baidu.com/s?tn=SE_baiduxueshu_c1gjeupa&ie=utf-8&sc_hit=1'
         res = self.ss.get(url, params={
             "wd": q
